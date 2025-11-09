db48f155e04cc60a9006546dda40ac485d1eb3b9
Ghaitsa Tsuraya Shofa <ghaitsatsurayashofa@gmail.com>
2025-11-09 22:02:03 +0700

    utsalpro

---
+++ Variabel.java
@@ -0,0 +1,20 @@
+public class Variabel{
+    public static void main(String[] args){
+        String nama = "Ghaitsa Tsuraya Shofa";
+        String tempatTanggalLahir = "Sukabumi, 09 Agustus 2006";
+        String jenisKelamin = "Perempuan";
+        String alamatLengkap = "Sukabumi, Benda";
+        String noHp = "0821256572";
+        double penghasilanSebulan = 2900000;
+
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap : " + nama);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin : " + jenisKelamin);
+        System.out.println("Alamat Lengkap : " + alamatLengkap);
+        System.out.println("No Hp : " + noHp);
+        System.out.println("Penghasilan Sebulan :Rp" + penghasilanSebulan);
+
+    }
+}
