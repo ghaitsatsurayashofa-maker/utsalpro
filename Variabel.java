34a8063c2ede3679b180db5d7d9333d923201bde
Ghaitsa Tsuraya Shofa <ghaitsatsurayashofa@gmail.com>
2025-11-10 20:57:02 +0700

    utsalpro

--- Variabel.java
+++ Variabel.java
@@ -3,17 +3,17 @@ public class Variabel{
         String nama = "Ghaitsa Tsuraya Shofa";
         String tempatTanggalLahir = "Sukabumi, 09 Agustus 2006";
         String jenisKelamin = "Perempuan";
-        String alamatLengkap = "Cicurug, Desa Benda";
-        String noHp = "0821536782";
+        String alamat = "Cicurug, Desa Benda";
+        String noHp = "0821637282";
         double penghasilanSebulan = 2900000;
 
         System.out.println("Assalamu'alaikum");
         System.out.println("Izinkan saya memperkenalkan diri");
-        System.out.println("Nama Lengkap : " + nama);
-        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
-        System.out.println("Jenis Kelamin : " + jenisKelamin);
-        System.out.println("Alamat Lengkap : " + alamatLengkap);
-        System.out.println("No Hp : " + noHp);
+        System.out.println("Nama Lengkap :" + nama);
+        System.out.println("Tempat dan Tanggal Lahir :" + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin :" + jenisKelamin);
+        System.out.println("Alamat Lengkap :" + alamat);
+        System.out.println("No Hp :" + noHp);
         System.out.println("Penghasilan Sebulan :Rp" + penghasilanSebulan);
 
     }
